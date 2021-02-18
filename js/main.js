10450db3b5540d33aaa2a63a2022f4a2ba2b69b9
Salvatore Tidu <salvatoret9@outlook.it>
2021-02-18 18:28:55 +0100

    JS_Pwdgen_Wannabe

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// var first_name = "salvatore";
+
+// salvatore = prompt("Enter your First Name");
+// console.log(first_name);
+
+
+// var last_name = "tidu";
+
+// tidu = prompt("Enter your Last Name");
+// console.log(last_name);
+
+
+// var favorite_color = "red";
+
+// red = prompt("Enter your Favorite Color");
+// console.log(favorite_color);
+
+
+prompt("Enter your First Name" + "," + " your Last Name" + " and" + " your Favorite Color");
+
+document.getElementById("my_id").innerHTML = "salvatore" + "tidu" + "red" + 19;
+
